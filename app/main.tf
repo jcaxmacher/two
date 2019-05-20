430f5d9b79f3ad4cfdc716cc3325214622f8f081
Jeremy Axmacher <jeremy@obsoleter.com>
2019-05-19 21:19:53 -0400

    add new vpc

--- app/main.tf
+++ app/main.tf
@@ -5,6 +5,18 @@ variable "private_key" {
   default = "c:/Users/jeremy/.ssh/jca-us-east-1.pem"
 }
 
+variable "network_address_space" {
+  default  = "10.1.0.0/16"
+}
+
+variable "subnet1_address_space" {
+  default =   "10.1.0.0/24"
+}
+
+variable "subnet2_address_space" {
+default =  "10.1.1.0/24"
+}
+
 provider "aws" {
   region                  = "us-east-1"
   version = "2.10"
@@ -34,11 +46,72 @@ data "aws_ami" "amzn_linux" {
   owners = ["137112412989"]
 }
 
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.network_address_space}"
+  enable_dns_hostnames = "true"
+}
+  
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${var.subnet1_address_space}"
+  vpc_id = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+}
+
+resource "aws_route_table_association" "rta-subnet1" {
+  subnet_id = "${aws_subnet.subnet1.id}"
+  route_table_id = "${aws_route_table.rtb.id}"
+}
+
+resource "aws_security_group" "nginx-sg" {
+  name = "nginx_sg"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "nginx1" {
   ami                         = "${data.aws_ami.amzn_linux.id}"
   associate_public_ip_address = true
   instance_type               = "t3.micro"
   key_name                    = "jca-us-east-1"
+  vpc_security_group_ids      = ["${aws_security_group.nginx-sg.id}"]
+  subnet_id                   = "${aws_subnet.subnet1.id}"
+
 
   connection {
     type = "ssh"
